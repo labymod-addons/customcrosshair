afba1db2e688b59e814c81224c50b8dd4d32e130
Christian <r0bby@gmx.net>
2021-11-25 20:59:49 +0100

    Update template

--- core/src/main/java/org/example/core/ExampleAddon.java
+++ core/src/main/java/org/example/core/ExampleAddon.java
@@ -1,20 +1,18 @@
 package org.example.core;
 
-import net.labymod.api.client.event.TickEvent;
 import net.labymod.api.event.Subscribe;
-import net.labymod.api.models.addon.annotation.AddonInfo;
+import net.labymod.api.event.client.lifecycle.GameTickEvent;
 import net.labymod.api.models.addon.annotation.AddonMain;
 import org.jetbrains.annotations.NotNull;
 
 @AddonMain
-@AddonInfo(id = "example", name = "Example")
 public class ExampleAddon {
 
   public ExampleAddon() {
   }
 
   @Subscribe
-  public void onTick(@NotNull TickEvent event) {
+  public void onTick(@NotNull GameTickEvent event) {
     System.out.println("Hello, Minecraft Tick (" + event.getPhase().name() + ")");
   }
 
