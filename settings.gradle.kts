b49d4ef9f5cf6c114d7dd374222bcf4a9b6a4f86
Christian <r0bby@gmx.net>
2026-06-03 13:58:51 +0200

    Update LabyGradle to 0.8.0, sync CI workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "customcrosshair-addon"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.5.9"
+    val labyGradlePluginVersion = "0.8.0"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
